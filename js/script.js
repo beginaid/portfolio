cb837c14880f9049fd15642a338e6c92b4341d13
zookao <zuka.info.official@gmail.com>
2019-11-23 11:58:06 +0900

    modify image size

--- js/script.js
+++ js/script.js
@@ -48,13 +48,3 @@ $(function () {
     },
   });
 });
-
-window.addEventListener('DOMContentLoaded', function () {
-  var swiper = new Swiper('.sample01 .swiper-container', {
-    pagination: '.swiper-pagination',
-    paginationClickable: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    loop: true
-  });
-}, false);
