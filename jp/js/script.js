2bb3332013402c01eada2a6cae4e97e3fb730967
zookao <zuka.info.official@gmail.com>
2019-11-24 16:51:10 +0900

    modify japanese

--- jp/js/script.js
+++ jp/js/script.js
@@ -33,18 +33,3 @@ $(function () {
   });
 
 });
-
-$(function () {
-  let swiper = new Swiper('.swiper-container', {
-
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-    },
-  });
-});
